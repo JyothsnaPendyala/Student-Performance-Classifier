0e6b64c72ba0360e70c691dec7594c9d8697de90
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-02-13 17:36:53 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -2,12 +2,7 @@ import pandas as pd
 import os
 import subprocess
 
-#os.system("sudo apt-get update")
-#os.system("sudo apt-get install git-secret")
 
-#hostname = subprocess.check_output(['git', 'secret', 'reveal', '-p', 'DB_HOSTNAME']).decode().strip()
-hostname = os.environ['DB_HOSTNAME']
-print(hostname)
 def load_data():
     data = pd.read_csv('data.csv')
     print(data.head())
