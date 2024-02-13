a14a2eb3aced053a24d06fb1c840dd5c1d27fe6d
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-02-13 11:52:46 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -2,8 +2,8 @@ import pandas as pd
 import os
 import subprocess
 
-os.environ("sudo apt-get update")
-os.environ("sudo apt-get install git-secret")
+os.system("sudo apt-get update")
+os.system("sudo apt-get install git-secret")
 
 hostname = subprocess.check_output(['git', 'secret', 'reveal', '-p', 'DB_HOSTNAME']).decode().strip()
 #hostname = os.environ.get('DB_HOSTNAME')
